4406532ecaba0e3d8067ee3e19f6bff307e9b0ec
Norbert Cyrklaff <the.last.of.us.08.08@gmail.com>
2017-03-13 23:16:01 +0100

    Smart Pointers in development.

---
+++ Smart_Pointers/Smart_Pointers/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "unique_ptr.h"
+
+int main(int argc, char *argv[])
+{
+    UNIQUE_MEELOGIC::Start();
+    return 0;
+}
---
+++ Smart_Pointers/Smart_Pointers/unique_ptr.h
@@ -0,0 +1,13 @@
+#ifndef UNIQUE_PTR_H
+#define UNIQUE_PTR_H
+
+namespace UNIQUE_MEELOGIC
+{
+    void Unique_Ptr_Interface();
+    void Unique_Ptr_Interface_Extended();
+
+    /* Passing pointers: */
+    void Start();
+}
+
+#endif // UNIQUE_PTR_H
