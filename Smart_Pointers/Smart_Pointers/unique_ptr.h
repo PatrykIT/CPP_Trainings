7fe841619ad653b7becfda3d343749f5ec8e45a6
Norbert Cyrklaff <the.last.of.us.08.08@gmail.com>
2017-03-13 23:36:46 +0100

    To do comments.

--- Smart_Pointers/Smart_Pointers/unique_ptr.h
+++ Smart_Pointers/Smart_Pointers/unique_ptr.h
@@ -6,7 +6,10 @@ namespace UNIQUE_MEELOGIC
     void Unique_Ptr_Interface();
     void Unique_Ptr_Interface_Extended();
 
-    /* Passing pointers: */
+    // Watch out for this: http://stackoverflow.com/questions/16894400/how-to-declare-stdunique-ptr-and-what-is-the-use-of-it
+    // Wise words: http://stackoverflow.com/questions/25370628/doesnt-get-break-the-idea-behind-stdunique-ptr
+    // Passing pointers: http://stackoverflow.com/questions/8114276/how-do-i-pass-a-unique-ptr-argument-to-a-constructor-or-a-function?rq=1
+
     void Start();
 }
 
