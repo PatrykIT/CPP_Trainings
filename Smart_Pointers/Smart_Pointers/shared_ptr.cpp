86e6876d802fef04d0a2027dee75138c7bf7e470
Norbert Cyrklaff <the.last.of.us.08.08@gmail.com>
2017-03-27 12:31:14 +0200

    use_count example

--- Smart_Pointers/Smart_Pointers/shared_ptr.cpp
+++ Smart_Pointers/Smart_Pointers/shared_ptr.cpp
@@ -4,21 +4,28 @@
 
 void SHARED_MEELOGIC::Start()
 {
-
+    Shared_Ptr_Contructors_Custom_Class_1();
 }
 
 
 void SHARED_MEELOGIC::Shared_Ptr_Contructors_Custom_Class_1()
 {
-    std::shared_ptr<Objects> object_ptr_one = std::make_shared<Objects>();
+    std::shared_ptr<Objects> object_ptr_one = std::make_shared<Objects>(5);
     std::shared_ptr<Objects> object_ptr_two (object_ptr_one);
+
+    long shared_pointers_in_use = object_ptr_one.use_count();
+    std::cout << "Shared pointers in use: " << shared_pointers_in_use << "\n";
 
 }
 
 SHARED_MEELOGIC::Objects::Objects() { std::cout << "Constructor called.\n"; }
 SHARED_MEELOGIC::Objects::Objects(int nr) { x = nr; std::cout << "Constructor with parameter called.\n"; }
 SHARED_MEELOGIC::Objects::~Objects() { std::cout << "Destructor called.\n"; }
+
 SHARED_MEELOGIC::Objects::Objects(const Objects &other) { std::cout << "Copy constructor called.\n"; std::ignore = other; }
-SHARED_MEELOGIC::Objects& SHARED_MEELOGIC::Objects::operator=(const Objects &other) { std::cout << "Assignment constructor called.\n"; std::ignore = other; return *this;}
+SHARED_MEELOGIC::Objects& SHARED_MEELOGIC::Objects::operator=(const Objects &other)
+{ std::cout << "Assignment constructor called.\n"; std::ignore = other; return *this;}
+
 SHARED_MEELOGIC::Objects::Objects(Objects &&other) { std::cout << "Move constructor called.\n"; std::ignore = other; }
-SHARED_MEELOGIC::Objects& SHARED_MEELOGIC::Objects::operator=(Objects &&other) { std::cout << "Move assignment constructor called.\n"; std::ignore = other; return *this; }
+SHARED_MEELOGIC::Objects& SHARED_MEELOGIC::Objects::operator=(Objects &&other)
+{ std::cout << "Move assignment constructor called.\n"; std::ignore = other; return *this; }
