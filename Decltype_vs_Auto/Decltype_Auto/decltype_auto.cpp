73ae3e14666a289054a09892f6973be3444be2b7
Norbert Cyrklaff <the.last.of.us.08.08@gmail.com>
2017-05-13 14:01:14 +0200

    Example of usage of decltype finished.

--- Decltype_vs_Auto/Decltype_Auto/decltype_auto.cpp
+++ Decltype_vs_Auto/Decltype_Auto/decltype_auto.cpp
@@ -6,9 +6,6 @@
 #include <cassert>
 
 
-/* http://stackoverflow.com/questions/24109737/what-are-some-uses-of-decltypeauto */
-/* http://stackoverflow.com/questions/21369113/what-is-the-difference-between-auto-and-decltypeauto-when-returning-from-a-fun */
-
 void MEELOGIC_DECLTYPE_VS_AUTO::Initialization_of_Local_Variables_Const()
 {
     std::cout << std::boolalpha; //Changes printing from 0 and 1 to false & true.
@@ -25,8 +22,6 @@ void MEELOGIC_DECLTYPE_VS_AUTO::Initialization_of_Local_Variables_Const()
 
     const auto number_auto_const = number;
     //std::cout << "Is const int: " << std::is_const<decltype(number_auto_const)>::value  << '\n';
-
-
 }
 
 void MEELOGIC_DECLTYPE_VS_AUTO::Initialization_of_Local_Variables_Reference()
@@ -34,15 +29,12 @@ void MEELOGIC_DECLTYPE_VS_AUTO::Initialization_of_Local_Variables_Reference()
     /* REFERENCE CASE */
     int number_2 = 10;
 
-    int &reference_to_number = number_2;
+    int& reference_to_number = number_2;
     auto reference_to_number_auto = reference_to_number;
-    decltype(reference_to_number) reference_to_number_decltype = reference_to_number;
-    decltype(auto) reference_to_number_decltype_2 = reference_to_number; //C++14
+    decltype(auto) reference_to_number_decltype = reference_to_number; //C++14
 
-    std::cout << "Is int& : " << std::is_same<int&, decltype(reference_to_number)>::value << "\n";
     std::cout << "Is int& : " << std::is_same<int&, decltype(reference_to_number_auto)>::value << "\n";
     std::cout << "Is int& : " << std::is_same<int&, decltype(reference_to_number_decltype)>::value << "\n";
-    std::cout << "Is int& : " << std::is_same<int&, decltype(reference_to_number_decltype_2)>::value << "\n";
 
     auto &reference_to_number_auto_fixed = reference_to_number;
     std::cout << "Is int& : " << std::is_same<int&, decltype(reference_to_number_auto_fixed)>::value << "\n";
@@ -80,86 +72,8 @@ void MEELOGIC_DECLTYPE_VS_AUTO::Vector_Evaluation_Decltype()
 
 
 
-int& return_number_by_reference(int &number)
-{
-    return number;
-}
 
-/* This straight-forward syntax is available since C++14.
- * On C++11 it wouldn't compile with error: "Missing trailing return type" */
-//auto Return_Number_Wrapper(int &number)
-//{
-//    return return_number_by_reference(number);
-//}
-
-
-/* This is how it needs to be done in C++11. */
-//auto Return_Number_Wrapper(int &number) -> decltype(return_number_by_reference(number))
-//{
-//    return return_number_by_reference(number);
-//}
-
-/* decltype(auto) feature is available since C++14.
- * This is the best, most generic way of returning a value that was passed from another function. */
-decltype(auto) Return_Number_Wrapper(int &number)
-{
-    return return_number_by_reference(number);
-}
-
-
-void MEELOGIC_DECLTYPE_VS_AUTO::Return_Value()
-{
-    int nr = 5;
-    decltype(Return_Number_Wrapper(nr)) number = Return_Number_Wrapper(nr);
-
-    std::cout << "Is int& : " << std::is_same<int&, decltype(number)>::value << "\n";
-    std::cout << "Is int : " << std::is_same<int, decltype(number)>::value << "\n";
-}
-
-
-
-
-
-const std::string& Modify_String(std::string word)
-{
-    std::string final_string = "Meelogic" + word.at(0) + word.at(1);
-    return final_string;
-}
-
-
-decltype(auto) Modify_String_Wrapper(std::string word)
-{
-    if(word.length() > 1)
-        return Modify_String(word);
-    else
-        throw std::length_error("String must have at least 2 characters.");
-}
-
-//auto Modify_String_Wrapper(std::string word)
-//{
-//    if(word.length() > 1)
-//        return Modify_String(word);
-//    else
-//        throw std::length_error("String must have at least 2 characters.");
-//}
-
-//auto Modify_String_Wrapper(std::string word) -> decltype(Modify_String(word))
-//{
-//    if(word.length() > 1)
-//        return Modify_String(word);
-//    else
-//        throw std::length_error("String must have at least 2 characters.");
-//}
-
-
-
-void MEELOGIC_DECLTYPE_VS_AUTO::Return_Value_String()
-{
-    std::string name = "AG";
-    //std::string output = Modify_String_Wrapper(name);
-    std::string output = Modify_String(name);
-}
-
+//http://stackoverflow.com/questions/918567/size-t-vs-containersize-type
 
 std::vector<int> global_numbers { 1, 5, 10 };
 
@@ -168,24 +82,48 @@ decltype(auto) Return_Middle()
     if(global_numbers.empty())
         throw std::range_error("Vector is empty!");
 
-    size_t index = global_numbers.size() / 2;
+    /* Which one is correct? */
+    //size_t index = global_numbers.size() / 2;
+    //int index = global_numbers.size() / 2;
+    //std::vector<int>::size_type index = global_numbers.size() / 2;
+
+    auto index = global_numbers.size() / 2;
     return global_numbers[index];
 }
 
-//auto Return_Middle(int index)
+//auto Return_Middle()
 //{
 //    if(global_numbers.empty())
 //        throw std::range_error("Vector is empty!");
 
-//    size_t index = global_numbers.size() / 2;
+//    auto index = global_numbers.size() / 2;
 //    return global_numbers[index];
 //}
 
+
+//int Return_Middle()
+//{
+//    if(global_numbers.empty())
+//        throw std::range_error("Vector is empty!");
+
+//    auto index = global_numbers.size() / 2;
+//    return global_numbers[index];
+//}
+
+
+//auto Return_Middle() -> decltype(global_numbers[0])
+//{
+//    if(global_numbers.empty())
+//        throw std::range_error("Vector is empty!");
+
+//    auto index = global_numbers.size() / 2;
+//    return global_numbers[index];
+//}
+
+
 void MEELOGIC_DECLTYPE_VS_AUTO::Return_Value_Vector()
 {
-    decltype(Return_Middle()) number_decltype = Return_Middle();
-    decltype(auto) number_decltype_2 = Return_Middle();
-
+    decltype(auto) number_decltype = Return_Middle();
     auto number_auto = Return_Middle();
 
     std::cout << std::is_same<int&, decltype(number_decltype)>::value << "\n";
@@ -209,7 +147,7 @@ namespace ANSWERS
 {
     /* Answer to exercise 1: */
     template <typename Type>
-    Type Function_with_Multiple_Returns_Answer_2()
+    Type Function_with_Multiple_Returns_Answer()
     {
         bool return_first = true;
         if(return_first)
@@ -222,7 +160,7 @@ namespace ANSWERS
 
 void MEELOGIC_DECLTYPE_VS_AUTO::Exercises()
 {
-    auto number_1 = ANSWERS::Function_with_Multiple_Returns_Answer_2<double>();
+    auto number_1 = ANSWERS::Function_with_Multiple_Returns_Answer<double>();
     std::cout << "Number 2: " << number_1 << "\n";
 }
 
